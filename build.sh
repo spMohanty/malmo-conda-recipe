2f8c1679e6dc3c8786113cefd7f0d4ff7f8fb0a5
S.P. Mohanty <spmohanty91@gmail.com>
2018-02-17 19:48:56 +0100

    Configuration for builds linux64 builds for py27, py35, py36

--- build.sh
+++ build.sh
@@ -13,6 +13,16 @@ cp -r ${SRC_DIR}/Schemas/ ${PREFIX}/Schemas/
 
 cp ${RECIPE_DIR}/extra_files/FindBoost.cmake ${SRC_DIR}/cmake/
 
+export PYTHON_VERSION="`python -c 'import platform; print(platform.python_version()[:3])'`"
+echo "Python Version ${PYTHON_VERSION}"
+
+# Custom changes for py35 and py36
+# TODO: Send pullrequest to Microsoft/Malmo
+if [ ${PY3K} -eq 1 ]; then
+  sed -i -e 's/date_time filesystem iostreams program_options python regex system/date_time filesystem iostreams program_options python3 regex system/g' CMakeLists.txt
+  sed -i -e 's/USE_PYTHON_VERSIONS 2.7/USE_PYTHON_VERSIONS ${PYTHON_VERSION}/g' CMakeLists.txt
+fi
+
 export MALMO_XSD_PATH=${PREFIX}/Schemas/
 
 cd ${SRC_DIR}
@@ -22,27 +32,60 @@ cd build
 echo "XSD_PATH = ${MALMO_XSD_PATH}"
 export TERM=${TERM:-dumb} #Dummy $TERM variable to make gradle happy
 
-cmake \
-  -DBUILD_DOCUMENTATION=Off \
-  -DINCLUDE_CSHARP=Off \
-  -DUSE_PYTHON_VERSIONS=${PYTHON_VERSION} \
-  -DXSD_EXECUTABLE=$PREFIX/bin/xsd-marlo \
-  -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -L${PREFIX}/lib -lxerces-c -Wunused-command-line-argument" \
-  -DBUILD_TYPE=Release \
-  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  ../
 
-make -j${CPU_COUNT}
-make install
 
-# Copy over Minecraft to env root
-cp -r ${SRC_DIR}/build/install/Minecraft ${PREFIX}/Minecraft
-# Copy over MalmoPython.so to python site packages
-cp ${SRC_DIR}/build/install/Python_Examples/MalmoPython.so ${SP_DIR}/
+if [ "$(uname)" == "Darwin" ]; then
 
-cp -r ${SRC_DIR}/build/install ${PREFIX}/install
-cp ${RECIPE_DIR}/extra_files/malmo-server ${PREFIX}/bin/malmo-server
-chmod +x ${PREFIX}/bin/malmo-server
+  cmake \
+    -DBUILD_DOCUMENTATION=Off \
+    -DINCLUDE_CSHARP=Off \
+    -DXSD_EXECUTABLE=$PREFIX/bin/xsd-marlo \
+    -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -L${PREFIX}/lib -lxerces-c -Wunused-command-line-argument" \
+    -DBUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DSTATIC_BOOST=On \
+    ../
 
+  make -j${CPU_COUNT}
+  make install
+
+  # Copy over Minecraft to env root
+  cp -r ${SRC_DIR}/build/install/Minecraft ${PREFIX}/Minecraft
+  # Copy over MalmoPython.so to python site packages
+  cp ${SRC_DIR}/build/install/Python_Examples/MalmoPython.so ${SP_DIR}/
+
+  cp -r ${SRC_DIR}/build/install ${PREFIX}/install
+  cp ${RECIPE_DIR}/extra_files/malmo-server ${PREFIX}/bin/malmo-server
+  chmod +x ${PREFIX}/bin/malmo-server
+
+fi
+
+if [ "$(uname)" == "Linux" ]; then
+  cmake \
+    -DBUILD_DOCUMENTATION=Off \
+    -DINCLUDE_CSHARP=Off \
+    -DINCLUDE_LUA=Off \
+    -DXSD_EXECUTABLE=$PREFIX/bin/xsd-marlo \
+    -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -L${PREFIX}/lib -lxerces-c -lpthread" \
+    -DBUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DSTATIC_BOOST=On \
+    -DBOOST_INCLUDEDIR=${PREFIX}/include \
+    -DBOOST_LIBRARYDIR=${PREFIX}/lib \
+    -DUSE_PYTHON_VERSIONS_DESC=${PYTHON_VERSION} \
+    ../
+
+  make -j${CPU_COUNT}
+  make install
+
+  # Copy over Minecraft to env root
+  cp -r ${SRC_DIR}/build/install/Minecraft ${PREFIX}/Minecraft
+  # Copy over MalmoPython.so to python site packages
+  cp ${SRC_DIR}/build/install/Python_Examples/MalmoPython.so ${SP_DIR}/
+
+  cp -r ${SRC_DIR}/build/install ${PREFIX}/install
+  cp ${RECIPE_DIR}/extra_files/malmo-server ${PREFIX}/bin/malmo-server
+  chmod +x ${PREFIX}/bin/malmo-server
+fi
 
 # exit 1
