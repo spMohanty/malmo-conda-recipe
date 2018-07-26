edf9c8eafe02bce177a29858f864c9943cda6972
flaurent <florian.laurent@gmail.com>
2018-07-26 19:10:11 +0200

    Fixing dependency issues, work in progress

--- bld.bat
+++ bld.bat
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 set INCLUDE_PATH=%PREFIX%\includes
 set LIBRARY_PATH=%PREFIX%\lib
 
@@ -26,9 +24,21 @@ mkdir build
 cd build
 
 @echo XSD_PATH = %MALMO_XSD_PATH%
-set TERM=%TERM%:-dumb 
+set TERM=%TERM%:-dumb
 
-cmake -DBUILD_DOCUMENTATION=Off -DINCLUDE_CSHARP=Off -DCMAKE_C_COMPILER=gcc -DCMAKE_INSTALL_PREFIX=%PREFIX% -DSTATIC_BOOST=On -DUSE_PYTHON_VERSIONS_DESC=%PYTHON_VERSION% ../
+@echo PATH : %PATH%
 
-make -j30
-make install
+xcopy /S /Q /Y /F %RECIPE_DIR%\extra_files\CMakeLists.txt %SRC_DIR%\CMakeLists.txt
+
+set JAVA_INCLUDE_PATH2=%PREFIX%\Library\include
+@echo JAVA_INCLUDE_PATH2 : %JAVA_INCLUDE_PATH2%
+
+REM sed -i -e "s/\"python3\"/\"python%CONDA_PY%\"/g" CMakeLists.txt
+REM  from : SET( BOOST_PYTHON_MODULE_NAME "python3" )
+REM  to : SET( BOOST_PYTHON_MODULE_NAME "python36" ) (or "python37") depending on the python build.
+REM  This lets FindBoost find where boost and boost_python are
+
+cmake -DBUILD_DOCUMENTATION=Off -DJAVA_INCLUDE_PATH2=%JAVA_INCLUDE_PATH2% -DINCLUDE_CSHARP=Off -DCMAKE_INSTALL_PREFIX=%PREFIX% -DSTATIC_BOOST=On -DUSE_PYTHON_VERSIONS_DESC=%PYTHON_VERSION% ../
+
+REM  -j%NUMBER_OF_PROCESSORS%
+cmake --build . --config Release --target install
